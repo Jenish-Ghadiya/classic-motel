dcd328d03faadff35d1267bd1de58c4d91f02b79
Jenish <jenishvghadiya@gmail.com>
2024-09-10 10:48:02 +0530

    initial commit

--- src/components/pages/classicfood/cardicon/index.jsx
+++ src/components/pages/classicfood/cardicon/index.jsx
@@ -3,7 +3,7 @@ import { IoCartOutline } from "react-icons/io5";
 import "./cardicon.scss";
 import { Link } from "react-router-dom";
 export default function CardIcon() {
-    const cartCount = JSON.parse(localStorage.getItem("CartData"));
+    const cartCount = JSON.parse(localStorage.getItem("CartData")) || [];
     
 
     return (
