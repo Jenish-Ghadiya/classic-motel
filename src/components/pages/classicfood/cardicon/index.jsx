d099e19c41519a518fab2b39a722f887da02df2c
Jenish <jenishvghadiya@gmail.com>
2024-09-30 10:45:22 +0530

    initial commit

--- src/components/pages/classicfood/cardicon/index.jsx
+++ src/components/pages/classicfood/cardicon/index.jsx
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import "./cardicon.scss";
 import { Link } from "react-router-dom";
+
 export default function CardIcon() {
-    const [count, setCount] = useState(JSON.parse(localStorage.getItem("CartData")) )
+    const [count, setCount] = useState(() => {
+        const cartData = localStorage.getItem("CartData");
+        return cartData ? JSON.parse(cartData).length : 0;
+    });
     
     useEffect(() => {
-        setCount(JSON.parse(localStorage.getItem("CartData")))
-    }, [count])
-    
+        const handleStorageChange = () => {
+            const cartData = localStorage.getItem("CartData");
+            setCount(cartData ? JSON.parse(cartData).length : 0);
+        };
 
+        // Check for changes every 100ms
+        const intervalId = setInterval(handleStorageChange, 100);
+
+        return () => clearInterval(intervalId);
+    }, []);
+    
     return (
         <div className="cardicon">
             <Link to="/cart">
                 <IoCartOutline className="icon" />
-                <div className="cart-count">{count.length}</div>
+                <div className="cart-count">{count}</div>
             </Link>
         </div>
     );
